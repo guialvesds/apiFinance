15a2e3ca9d6977c40c31a2f39b1077865d36dee4
guialvesds <guilherme_ealves@outlook.com>
2025-03-01 22:54:56 -0300

    Fix(User) Fixed routes for searching users by id and general

--- src/main/java/gads/web/financeOn/infrastructure/repository/TaskRepository.java
+++ src/main/java/gads/web/financeOn/infrastructure/repository/TaskRepository.java
@@ -1,9 +1,10 @@
 package gads.web.financeOn.infrastructure.repository;
 
-import gads.web.financeOn.infrastructure.entity.TaskEntity;
+import gads.web.financeOn.infrastructure.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository extends MongoRepository<TaskEntity, String> {
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+
 }
