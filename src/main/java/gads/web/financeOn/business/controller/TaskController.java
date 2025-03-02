66b2ece352ede521874aa6433cf5e0909e14b6b3
guialvesds <guilherme_ealves@outlook.com>
2025-03-01 21:46:33 -0300

    Feat(TaskController) Implemted Task Controller

--- src/main/java/gads/web/financeOn/business/controller/TaskController.java
+++ src/main/java/gads/web/financeOn/business/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/task")
+@RequestMapping("/api/v1/tasks")
 public class TaskController {
 
     private final TaskService taskService;
