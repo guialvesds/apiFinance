fae49e3bf2b29bdacce56f24ea0acf13ab3216a0
guialvesds <guilherme_ealves@outlook.com>
2025-03-04 22:17:42 -0300

    Chore(WalletSevice) Updating WalletService and WalletControlle

--- src/main/java/gads/web/financeOn/business/controller/WalletController.java
+++ src/main/java/gads/web/financeOn/business/controller/WalletController.java
@@ -4,9 +4,10 @@ import gads.web.financeOn.business.services.WalletService;
 import gads.web.financeOn.infrastructure.entity.UserEntity;
 import gads.web.financeOn.infrastructure.entity.WalletEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/wallets")
@@ -18,11 +19,30 @@ public class WalletController {
         this.walletService = walletService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<WalletEntity>> getAllWallets(){
+        var wallets = this.walletService.findAll();
+        return ResponseEntity.ok(wallets);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<WalletEntity> findById(@PathVariable String id){
+        var wallet = this.walletService.findById(id);
+        return ResponseEntity.ok(wallet);
+    }
+
     @PostMapping("/{id}")
     public ResponseEntity<WalletEntity> save(@RequestBody WalletEntity wallet, @PathVariable UserEntity id){
 
         wallet = this.walletService.save(wallet, id);
         return ResponseEntity.status(HttpStatus.CREATED).body(wallet);
+    }
+
+    // Build controi o objeto
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(String id) {
+        this.walletService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- src/main/java/gads/web/financeOn/business/services/WalletService.java
+++ src/main/java/gads/web/financeOn/business/services/WalletService.java
@@ -1,10 +1,16 @@
 package gads.web.financeOn.business.services;
 
+import gads.web.financeOn.business.exceptions.BusinessException;
 import gads.web.financeOn.infrastructure.entity.UserEntity;
 import gads.web.financeOn.infrastructure.entity.WalletEntity;
 import gads.web.financeOn.infrastructure.repository.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class WalletService {
@@ -16,9 +22,22 @@ public class WalletService {
         this.walletRepository = walletRepository;
     }
 
+    public List<WalletEntity> findAll(){
+        return walletRepository.findAll();
+    }
+
+    public WalletEntity findById(String id){
+        return this.walletRepository.findById(id).orElseThrow(() -> new BusinessException("Wallet not found"));
+    }
+
     public WalletEntity save(WalletEntity wallet, UserEntity userId){
         wallet.setUser(userId);
         return walletRepository.save(wallet);
     }
+
+    public void delete(String userId){
+        this.walletRepository.deleteById(userId);
+    }
+
 
 }
